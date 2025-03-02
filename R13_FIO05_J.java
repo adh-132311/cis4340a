aa536a7d4ed745e29cbd33751ee399fa5d91ba35
adh-132311 <ad387277@ucf.edu>
2025-03-02 18:07:13 -0500

    Create User Selected R13_FIO05_J.java

---
+++ R13_FIO05_J.java
@@ -0,0 +1,13 @@
+
+final class Wrap {
+  private char[] dataArray;
+ 
+  public Wrap() {
+    dataArray = new char[10];
+    // Initialize
+  }
+ 
+  public CharBuffer getBufferCopy() {
+    return CharBuffer.wrap(dataArray).asReadOnlyBuffer();
+  }
+}
