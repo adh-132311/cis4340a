873031f15fc4b588d6e73f054fa22a28cd286d03
adh-132311 <ad387277@ucf.edu>
2025-03-01 19:35:26 -0500

    Uploaded edited cis4340a.java file

---
+++ cis4340a.java
@@ -0,0 +1,23 @@
+/******************************************************************************
+ *  Compilation:  javac cis4340a.java
+ *  Execution:    java cis4340a
+ *
+ *  Prints 
+ *    "Hello, World - from the class of CIS4340 - Fall 2022 -UCF _YoUR NAME HERE_".
+ *  By tradition, this is everyone's first program.
+ *
+ * Revision History:
+ *   % java cis4360a
+ *  Hello, World - from the class of CIS4340 - Spring 2025 - UCF
+ *  %
+ *
+ ******************************************************************************/
+
+public class Cis4340a {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World - from the class of CIS4340 - Spring 2025 - UCF - Adam Holden");
+    }
+
+}
